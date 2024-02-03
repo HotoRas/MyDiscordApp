4446ea96820273bab0d589e24c7ea816143898f0
Hoto Ras <jdh59681@gmail.com>
2024-02-04 03:38:29 +0900

    epoch
    인데 굳이 날짜 계산을 하고 앉아있어야 되냐? 걍 24*60*60 빼면 하루 전 아니냐?

--- src/services/date/DateService.ts
+++ src/services/date/DateService.ts
@@ -3,23 +3,5 @@ export function isSameDate(a: Date, b: Date): boolean {
 }
 
 export function getYesterday(): Date {
-    const today: Date = new Date(Date.now())
-    let yyyy: number = today.getFullYear()
-    let mm: number = today.getMonth()
-    let dd: number = today.getDate()
-    dd -= 1
-    if (dd === 0) {
-        mm -= 1
-        if (mm === 0) {
-            yyyy -= 1
-            mm = 12
-        }
-        if (mm === 4 || mm === 6 || mm === 9 || mm === 11) dd = 30
-        else if (mm === 2) {
-            if (yyyy % 4 === 0 && (yyyy % 100 !== 0 || yyyy % 400 === 0)) mm = 29
-            else { mm = 28 }
-        }
-        else mm = 30
-    }
-    return new Date(yyyy, mm, dd)
+    return new Date(Date.now() - 24 * 60 * 60)
 }
